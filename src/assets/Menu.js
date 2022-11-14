b6e79c39690c3c743954fcece12e5d322b851d39
Sumiya-Sayeed <sumiyasayeed@gmail.com>
2022-11-14 20:16:12 +0600

    add static data

---
+++ src/assets/Menu.js
@@ -0,0 +1,24 @@
+const options = [
+    {
+        menu: 'Intro',
+        path: 'Intro',
+    },
+    {
+        menu: 'Experience',
+        path: 'Experience',
+    },
+    {
+        menu: 'Projects',
+        path: 'Projects',
+    },
+    {
+        menu: 'Skills',
+        path: 'Skills',
+    },
+    {
+        menu: 'Education',
+        path: 'Education',
+    },
+];
+
+export default options;
