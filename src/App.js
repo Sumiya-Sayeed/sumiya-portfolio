34bb603c3b2716af0463b9618ba4a1e5e7188268
Sumiya-Sayeed <sumiyasayeed@gmail.com>
2022-11-14 20:16:01 +0600

    add theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { withStyles } from '@mui/styles';
+import { CssBaseline } from '@mui/material';
+import Layout from './components/Layout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#0D4C92', //lighter green
+			dark: '#1b5e20', //darker green
+			light: '#e8f5e9' //lightest green for poppers
+		},
+		secondary: {
+			main: '#f2f9fb',
+			dark: '#37474f', //darker color for font
+		},
+	},
+	typography: {
+		fontFamily: "sans-serif",
+		useNextVariants: true,
+	},
+});
+
+const styles = {
+	'@global': {
+		'html, body, #root': {
+			width: '100%',
+			maxWidth: '100vw',
+			height: 'auto',
+			minHeight: '100vh',
+			// overflowX: 'hidden',
+			padding: 0,
+			margin: 0,
+			backgroundColor: '#eceff1',
+		},
+	},
+};
+
+const App = () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<Layout />
+		</ThemeProvider>
+	)
+};
+
+export default withStyles(styles)(App);
