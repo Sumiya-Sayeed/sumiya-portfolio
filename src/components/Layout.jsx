6ffaaa1f648b04c7ab66a43e9318a01c75f980ac
Sumiya-Sayeed <sumiyasayeed@gmail.com>
2022-11-15 13:50:25 +0600

    add fab button

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, {
+    useState
+} from "react";
 import {
-    Grid
+    Fab,
+    Grid,
+    Link,
+    Typography,
+    useTheme
 } from '@mui/material';
+import {
+    ArrowUpward
+} from '@mui/icons-material';
 import Appbar from './appbar';
 import Intro from './Intro';
 import Skills from './Skills';
@@ -11,6 +20,29 @@ import Education from "./Education";
 
 const Layout = () => {
     document.title = "Sumiya Sayeed";
+    const theme = useTheme();
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300) {
+            setVisible(true)
+        }
+        else if (scrolled <= 300) {
+            setVisible(false)
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+            /* you can also use 'auto' behaviour
+               in place of 'smooth' */
+        });
+    };
+
+    window.addEventListener('scroll', toggleVisible);
 
     return (
         <>
@@ -26,11 +58,53 @@ const Layout = () => {
                     height: '100%'
                 }}
             >
+                {
+                    window.pageYOffset > 0
+                    &&
+                    <Fab
+                        sx={{
+                            margin: 0,
+                            top: 'auto',
+                            right: 40,
+                            bottom: 40,
+                            left: 'auto',
+                            position: 'fixed',
+                            display: visible ? 'inline' : 'none',
+                            backgroundColor: 'white',
+                            color: theme.palette.primary.main,
+                            ':hover': {
+                                backgroundColor: 'white',
+                                color: theme.palette.primary.main,
+                                boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px'
+                            }
+                        }}
+                        onClick={scrollToTop}
+                    >
+                        <ArrowUpward
+                            sx={{
+                                marginTop: '7px',
+                                ':hover': {
+                                    height: '1.2em',
+                                    width: '1.2em'
+                                }
+                            }}
+                        />
+                    </Fab>
+                }
                 <Intro id="Intro" />
                 <Skills id="Skills" />
                 <Projects id="Projects" />
                 <Experience id="Experience" />
                 <Education id="Education" />
+                <Typography
+                    gutterBottom
+                    color={theme.palette.primary.main}
+                >
+                    Made by <Link
+                        underline="hover"
+                        href="https://www.linkedin.com/in/sumiya-sayeed-04a9319b/"
+                    >Sumiya Sayeed</Link>
+                </Typography>
             </Grid>
         </>
     )
